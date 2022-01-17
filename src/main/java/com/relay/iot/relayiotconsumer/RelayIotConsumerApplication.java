cee8db033155211dde81d28924502eb6d0ab12a7
Kunal Malhotra <kunal.malhotra@idemia.com>
2022-01-17 13:04:58 +0530

    Initial Project Structure

---
+++ src/main/java/com/relay/iot/relayiotconsumer/RelayIotConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.relay.iot.relayiotconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelayIotConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelayIotConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/relay/iot/relayiotconsumer/RelayIotConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.relay.iot.relayiotconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelayIotConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
